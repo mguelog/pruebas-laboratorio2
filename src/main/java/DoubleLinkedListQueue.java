8b872116223667ef44719ef264fb647c613232fd
Miguel Oña Guerrero <miguelog3295@outlook.com>
2022-03-14 19:15:06 +0100

    Add classes

---
+++ src/main/java/DoubleLinkedListQueue.java
@@ -0,0 +1,38 @@
+public class DoubleLinkedListQueue implements DoubleEndedQueue{
+
+
+    @Override
+    public void append(DequeNode node) {
+
+    }
+
+    @Override
+    public void appendLeft(DequeNode node) {
+
+    }
+
+    @Override
+    public void deleteFirst() {
+
+    }
+
+    @Override
+    public void deleteLast() {
+
+    }
+
+    @Override
+    public DequeNode peekFirst() {
+        return null;
+    }
+
+    @Override
+    public DequeNode peekLast() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
