9109c6287997555dcaa3ed04bcf5c7ca0bdec1ac
Miguel Oña Guerrero <miguelog3295@outlook.com>
2022-03-15 11:24:36 +0100

    Make append

--- src/main/java/org/pruebas/DoubleLinkedListQueue.java
+++ src/main/java/org/pruebas/DoubleLinkedListQueue.java
@@ -2,15 +2,50 @@ package org.pruebas;
 
 public class DoubleLinkedListQueue implements DoubleEndedQueue{
 
+    private DequeNode first;
+    private DequeNode last;
+    private int size;
+
+    public DoubleLinkedListQueue(){
+        this.first = null;
+        this.last = null;
+        size = 0;
+    }
 
     @Override
     public void append(DequeNode node) {
+        if(node == null){
+            throw new IllegalArgumentException("Null node");
+        }else if (size == 0){
+            this.first = node;
+            this.last = node;
+            size++;
+        }else {
+            this.last.setNext(node);
+            node.setPrevious(this.last);
+            node.setNext(null);
 
+            this.last=node;
+            size++;
+        }
     }
 
     @Override
     public void appendLeft(DequeNode node) {
+        if(node == null){
+            throw new IllegalArgumentException("Null node");
+        }else if(size == 0){
+            this.first = node;
+            this.last = node;
+            size++;
+        }else {
+            this.first.setPrevious(node);
+            node.setNext(this.first);
+            node.setPrevious(null);
 
+            this.first=node;
+            size++;
+        }
     }
 
     @Override
